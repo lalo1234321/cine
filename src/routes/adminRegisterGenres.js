af5471362a73d1563b89b203ff8441f1845af0f6
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-11-26 12:50:51 -0600

    minimum changes

--- src/routes/adminRegisterGenres.js
+++ src/routes/adminRegisterGenres.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const BD = require('../config/oracle.js');
 const {verifyToken} = require('../middlewares/isAuth');
-
+//corrgir
 router.post('/registerGenre',[verifyToken],(req,res)=>{
     let message = req.body.message;
     res.status(200).json({
