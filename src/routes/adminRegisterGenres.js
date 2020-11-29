8a0c67e32ae2512e9bd58bbf2fa882eb4ff9cbf8
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-11-28 21:36:39 -0600

    Post method for creating a new gender finished

--- src/routes/adminRegisterGenres.js
+++ src/routes/adminRegisterGenres.js
@@ -3,12 +3,23 @@ const router = Router();
 const BD = require('../config/oracle.js');
 const {verifyToken,verifyAdmin} = require('../middlewares/isAuth');
 //corrgir
-router.post('/registerGenre',[verifyToken,verifyAdmin],(req,res)=>{
-    let message = req.body.message;
-    res.status(200).json({
-        ok:true,
-        message
-    });
+router.post('/registerGenre',[verifyToken,verifyAdmin],async(req,res)=>{
+    let {idGender, name} = req.body;
+    try {
+        sql = "insert into gender values(:idGender,:name)";
+        await BD.Open(sql,[idGender,name],true);
+        res.status(200).json({
+            ok:true,
+            message: 'género creado con éxito en la base de datos'
+        })
+    } catch (error) {
+        res.status(500).json({
+            error
+        })
+    }
+    
+
+    
 
 });
 
