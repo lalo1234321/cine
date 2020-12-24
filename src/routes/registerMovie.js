4129539b069875da3cb8f0d569f2d7366451773d
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-12-23 19:45:31 -0600

    adding getMovies and getMovie by name

--- src/routes/registerMovie.js
+++ src/routes/registerMovie.js
@@ -3,6 +3,7 @@ const {Router} = require('express');
 const router = Router();
 const BD = require('../config/oracle.js');
 const {verifyToken,verifyAdmin} = require('../middlewares/isAuth');
+const { route } = require('./login.js');
 
 router.post('/registerMovie',[verifyToken,verifyAdmin], async (req,res) => {
     let {idGender, duration, name, posterImage} = req.body;
@@ -23,9 +24,25 @@ router.post('/registerMovie',[verifyToken,verifyAdmin], async (req,res) => {
 
 });
 
+router.get('/getMovieByName/:name',[verifyToken,verifyAdmin], async (req,res) => {
+    let name = req.params.name;
+    console.log(name);
+    sql = "select m.idMovie, m.duration, m.name, m.posterImage,g.name from movie m join gender g using (idGender) where m.name=:name";
+    let result = await BD.Open(sql,[name],true);
+    res.json({
+        Registros:result.rows
+    });
+});
 
+router.get('/getAllMovies',[verifyToken,verifyAdmin], async (req, res) => {
+    sql = "select m.idMovie, m.duration, m.name, m.posterImage,g.name from movie m inner join gender g on (m.idgender=g.idGender)";
+    let result = await BD.Open(sql, [], true);
+    res.json({
+        Registros:result.rows
+    });
+});
 
 
 
 module.exports = router;
-
+//select m.idMovie, m.duration, m.name, m.posterImage, g.name from movie m join gender g using (idGender) where m.name='Risa1';
