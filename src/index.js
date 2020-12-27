1cee97be56a668ca5e4e6d9db490bf7305203ffb
Sergio-DC <sgdc377@gmail.com>
2020-12-26 20:32:41 -0600

    refactoring api server

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-//imports
-//const empleado = require('./routes/empleado');
-const users = require('./routes/users');
-const login = require('./routes/login');
-const genre = require('./routes/gender');
-const movie = require('./routes/movie');
-const cinema = require('./routes/cinema');
-const movieLobby = require('./routes/movieLobby');
 //settings
 app.set('port', process.env.PORT);
 
@@ -20,13 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-//app.use(empleado);
-app.use(users);
-app.use(login);
-app.use(genre);
-app.use(movie);
-app.use(cinema);
-app.use(movieLobby);
+require('./routes/index')(app);
 //run
 app.listen(app.get('port'), () => {
     console.log(`Server on Port ${process.env.PORT}`);
